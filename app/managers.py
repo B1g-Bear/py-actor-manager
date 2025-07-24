53cd5394aecb5aa484764900a5afa94a3782c0c4
Andrii Novikov <forytube2023@gmail.com>
2025-07-24 14:22:26 +0300

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,50 @@
 import sqlite3
+from models import Actor
 
-from app.models import Actor
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str):
+        if not table_name.endswith("s"):
+            raise ValueError("Table name should be in plural form")
 
+        self.db_name = db_name
+        self.table_name = table_name
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        self._create_table()
 
-# add manager here
+    def _create_table(self):
+        self.cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS {self.table_name} (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL
+            )
+        """)
+        self.conn.commit()
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self.cursor.execute(f"""
+            INSERT INTO {self.table_name} (first_name, last_name)
+            VALUES (?, ?)
+        """, (first_name, last_name))
+        self.conn.commit()
+
+    def all(self) -> list[Actor]:
+        self.cursor.execute(f"SELECT * FROM {self.table_name}")
+        rows = self.cursor.fetchall()
+        return [Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows]
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        self.cursor.execute(f"""
+            UPDATE {self.table_name}
+            SET first_name = ?, last_name = ?
+            WHERE id = ?
+        """, (new_first_name, new_last_name, pk))
+        self.conn.commit()
+
+    def delete(self, pk: int) -> None:
+        self.cursor.execute(f"DELETE FROM {self.table_name} WHERE id = ?", (pk,))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
